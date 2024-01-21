fec4f91f54b3241f129ae26411c52f783569aa99
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-01-21 11:28:53 +0000

    Deploying to gh-pages from @ neromaxor/goit-js-hw-12@c8e3fc0d603dfbbc2a738ed45656a74083d4fd08 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,26 @@
-import{i as d,S as v,a as g}from"./assets/vendor-89feecc5.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&r(c)}).observe(document,{childList:!0,subtree:!0});function a(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerpolicy&&(s.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?s.credentials="include":t.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(t){if(t.ep)return;t.ep=!0;const s=a(t);fetch(t.href,s)}})();const w="https://pixabay.com/api/",b="41633959-4ab3a3c79df0d7e6ffc2251eb",L=document.querySelector(".form"),u=document.querySelector(".photo-container"),i=document.querySelector(".loader"),l=document.querySelector(".load-more");let p,n=1,m="",y=0,h=0;l.style.display="none";const P=()=>{const e=new URL(w);return e.searchParams.append("key",b),e.searchParams.append("image_type","photo"),e.searchParams.append("orientation","horizontal"),e.searchParams.append("safesearch","true"),e.searchParams.append("page",n),e.searchParams.append("per_page",40),e};L.addEventListener("submit",async e=>{e.preventDefault();const o=e.currentTarget.elements[0].value;if(o.length<4)alert("Sorry, Yours length is not enough. Min 4 letters.");else{i.style.display="block",l.style.display="none",m=o.toLowerCase(),n=1;try{await f()}catch(a){console.error(a)}}});l.addEventListener("click",async()=>{i.style.display="block",n++;try{await f()}catch(e){console.error(e)}});const k=async()=>{const e=P();e.searchParams.append("q",m);try{const o=await g.get(e.toString());if(o.status===200)return o.data;throw new Error("Request is not okay")}catch(o){console.error(o)}},f=async()=>{try{const e=await k();if(e.hits.length===0&&n===1){d.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),i.style.display="none";return}else if(e.hits.length===0){d.info({message:"We're sorry, but you've reached the end of search results.",position:"topRight"}),i.style.display="none",l.style.display="none";return}if(y=e.totalHits,n===1){const a=document.querySelector(".photo-card");a&&(h=a.getBoundingClientRect().height)}const o=e.hits.reduce((a,r,t)=>a+`<div class="photo-card">
-                    <div class="photo">
-                        <a href="${r.largeImageURL}" data-lightbox="gallery-${t}">
-                            <img src="${r.webformatURL}" alt="${r.tags}" width="360" height="200" />
-                        </a>
-                    </div>
-                    <div class="info-container">
-                        <div class="label-value">
-                            <div class="label-likes">Likes</div>
-                            <div class="value">${r.likes}</div>
-                        </div>
-                        <div class="label-value">
-                            <div class="label-likes">Views</div>
-                            <div class="value">${r.views}</div>
-                        </div>
-                        <div class="label-value">
-                            <div class="label-likes">Comments</div>
-                            <div class="value">${r.comments}</div>
-                        </div>
-                        <div class="label-value">
-                            <div class="label-likes">Downloads</div>
-                            <div class="value">${r.downloads}</div>
-                        </div>
-                    </div>
-                </div>`,"");n===1?u.innerHTML=o:u.innerHTML+=o,i.style.display="none",l.style.display=n*40>=y?"none":"block",p=new v(".photo a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250}),p.refresh(),window.scrollBy({top:h*2,behavior:"smooth"})}catch(e){console.error(e)}};window.scrollBy(0,window.innerHeight);window.scrollBy(0,-window.innerHeight);window.scrollBy({top:100,left:100,behavior:"smooth"});
+import{a as v,i as p,S as g}from"./assets/vendor-c145bea9.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function s(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerpolicy&&(o.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?o.credentials="include":t.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(t){if(t.ep)return;t.ep=!0;const o=s(t);fetch(t.href,o)}})();const b=v.create({baseURL:"https://pixabay.com/api/",params:{key:"41633959-4ab3a3c79df0d7e6ffc2251eb",image_type:"photo",orientation:"horizontal",safesearch:"true"}}),w=document.querySelector(".form"),u=document.querySelector(".photo-container"),f=document.querySelector(".loader"),l=document.querySelector(".button-load-more");let h,i=1,y=40,c="",d=1;const L=async(r="")=>{try{const e=await b.get("",{params:{q:r,page:i,per_page:y}});if(e.status===200)return d=Math.ceil(e.data.totalHits/y),e.data;throw new Error(`Request failed with status ${e.status}`)}catch(e){throw console.error("Error fetching data:",e),new Error("Error fetching data")}};function S(){var s;const e=2*(((s=document.querySelector(".photo-card"))==null?void 0:s.getBoundingClientRect().height)||0);window.scrollBy({top:e,behavior:"smooth"})}w.addEventListener("submit",async r=>{r.preventDefault();const e=r.currentTarget.elements[0].value;if(e.length<3)alert("Sorry, Yours length is not enough. Min 4 letters.");else try{c=e.toLowerCase(),i=1,await m(c)}catch(s){console.error(s)}});async function m(r){try{l.style.display="none";const e=await L(r);e.hits.length===0&&i===1&&p.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),i===1&&(u.innerHTML="");const s=e.hits.map((a,t)=>`<div class="photo-card">
+          <div class="photo">
+            <a href="${a.largeImageURL}" data-lightbox="gallery-${t}">
+              <img src="${a.webformatURL}" alt="${a.tags}" width="360" height="200" />
+            </a>
+          </div>
+          <div class="info">
+            <div class="label-value">
+              <div class="label">Likes</div>
+              <div class="value">${a.likes}</div>
+            </div>
+            <div class="label-value">
+              <div class="label">Views</div>
+              <div class="value">${a.views}</div>
+            </div>
+            <div class="label-value">
+              <div class="label">Comments</div>
+              <div class="value">${a.comments}</div>
+            </div>
+            <div class="label-value">
+              <div class="label">Downloads</div>
+              <div class="value">${a.downloads}</div>
+            </div>
+          </div>
+        </div>`).join("");u.insertAdjacentHTML("beforeend",s),h=new g(".photo a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250}),h.refresh(),i>1&&S(),i<d&&(l.style.display="inline")}catch(e){console.error(e)}}l.addEventListener("click",async()=>{i+=1,f.style.display="block";try{await m(c),i>=d&&(p.info({message:"We're sorry, but you've reached the end of search results.",position:"topRight"}),l.style.display="none")}catch(r){console.error(r)}finally{f.style.display="none"}});
 //# sourceMappingURL=commonHelpers.js.map
